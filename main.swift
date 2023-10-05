edb5283a2024c101e959a74ad56ff240a09abdd1
Phunyaphat Jamjaruen <phunyaphatjamja@gmail.com>
2023-10-05 20:56:35 +0700

    add other class

--- main.swift
+++ main.swift
@@ -17,20 +17,29 @@ struct Queue{
 
 
 //class
-class Employee{
-    static private var count:Int = 0
-    var id:Int
+class Person{
     var name:String
     var age:Int?
+
+    init(name:String,age:Int?){
+        self.name = name
+        self.age = age
+    }
+}
+
+
+class Employee:Person{
+    static private var count:Int = 0
+    var id:Int
     var salary:Double
 
     init(name:String,age:Int?,salary:Double){
         Employee.count += 1
         self.id = Employee.count
-        self.name = name
-        self.age = age
         self.salary = salary
+        super.init(name: name, age: age)
     }
+
 
     convenience init(name:String,salary:Double){
         self.init(name:name,age:nil,salary:salary)
@@ -41,7 +50,40 @@ class Employee{
     }
 }
 
-class Programmer:Employee{
-    static private var count:Int = 0 
+// class Programmer:Employee{
+//     static private var count:Int = 0 
+    
+// }
+
+class Seller:Employee{
+
+    var customers:[Customer] = []
+
+
+    override init(name:String,age:Int?,salary:Double){
+        super.init(name: name, age: age, salary: salary)
+    }
+
+}
+
+
+class Customer:Person{
+    static private var count = 0
+    let cust_id:Int
+    // var orders:[Orders] = []
+
+    init(name:String,age:Int){
+        Customer.count += 1
+        cust_id = Customer.count
+        super.init(name: name, age: age)
+    }
+    
+}
+
+struct Order{
+    static private var count = 0
+    var order_id:Int
+    var cust_id:Int
+    var orderDetails
     
 }
